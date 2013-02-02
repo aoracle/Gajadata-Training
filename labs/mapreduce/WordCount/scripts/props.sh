2d706d691ec1780628999873d5f3cbaae653a68a
Your Name <you@example.com>
2013-02-02 10:53:03 -0800

    2 files changed, 1 insertion(+), 2 deletions(-)  create mode 100644 temp  delete mode 100644 test hadoop@ubuntu:~/Gajadata-Training$ git config --global user.name "Amol Likhar" hadoop@ubuntu:~/Gajadata-Training$ git config --global user.email amol.likhar@gmail.com hadoop@ubuntu:~/Gajadata-Training$ git commit --amend --reset-author [master 72979e5] Remove test file and adding test temp file.  2 files changed, 1 insertion(+), 2 deletions(-) Remove test file and adding test temp file.

--- labs/mapreduce/WordCount/scripts/props.sh
+++ labs/mapreduce/WordCount/scripts/props.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export SAMPLEHOME=${HOME}/examples/MapReduce/WordCount
+export SAMPLEHOME=${HOME}/mapreduce_scripts/WordCount
 export INPUTDIR=${SAMPLEHOME}/data/input
 export OUTPUTDIR=${SAMPLEHOME}/data/output
 export DFSINPUT=${SAMPLEHOME}/data/input/dfsinput
